f355d6c328499c20640d5e86bb163e7300b186fa
Marian Sauer <marian.sauer@localhost>
2023-10-29 10:43:04 +0100

    Build cyfx3boot from source.

---
+++ toolchain_arm926ej-s.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY") 
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs -nostartfiles" )
+
+# here is the target environment is located
+SET(CMAKE_FIND_ROOT_PATH "/usr/lib/gcc/arm-none-eabi/10.3.1")
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
